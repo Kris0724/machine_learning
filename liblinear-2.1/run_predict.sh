ead15f9e625f6c155057e582716a3db28b88d2fc
Kris0724 <532299490@qq.com>
2017-02-03 17:32:41 +0800

    modify

--- liblinear-2.1/run_predict.sh
+++ liblinear-2.1/run_predict.sh
@@ -1,3 +1,4 @@
 
-./predict sort_samples_test.txt sort_samples_train2.txt.model pre.txt
+#./predict sort_samples_test.txt sort_samples_train2.txt.model pre.txt
+./predict sort_samples_train2.txt sort_samples_train2.txt.model pre2.txt
 
